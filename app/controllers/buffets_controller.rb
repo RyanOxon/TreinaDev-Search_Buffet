057fbc5ef3699367abae508903781d7ab4618aa8
Rafael Salgado <rafael.salgado31@gmail.com>
2024-04-20 09:37:05 -0300

    Usuario edita seu buffet

--- app/controllers/buffets_controller.rb
+++ app/controllers/buffets_controller.rb
@@ -1,17 +1,34 @@
 class BuffetsController < ApplicationController
-  before_action :set_buffet, only: :show
+  before_action :set_buffet, only: [:show, :edit, :update]
+  before_action :payment_methods, only: [:edit, :update, :new, :create]
 
   def show; end
+
+  def edit; end
+
+  def update
+    payment_methods_ids = params[:buffet][:payment_method_ids].reject(&:blank?)
+    if payment_methods_ids.any? && @buffet.update(buffet_params)
+      @buffet.buffet_payment_methods.destroy_all
+      payment_methods_ids.each do |id|
+        method = PaymentMethod.find(id)
+        BuffetPaymentMethod.create!(buffet: @buffet, payment_method: method)
+      end
+    redirect_to @buffet, notice: 'Buffet atualizado com sucesso'
+    else
+      @buffet.errors.add(:payment_method_ids, "não pode ficar em branco") unless payment_methods_ids.any?
+      flash.now[:alert] = "Erro ao atualizar Buffet"
+      render :edit
+    end
+  end
 
   def new
     redirect_to current_buffet_owner.buffet if current_buffet_owner.buffet
     @buffet = Buffet.new
-    @payment_methods = PaymentMethod.all
   end
 
   def create
     @buffet = Buffet.new(buffet_params)
-    @payment_methods = PaymentMethod.all
     @buffet.buffet_owner = current_buffet_owner
     payment_methods_ids = params[:buffet][:payment_method_ids].reject(&:blank?)
     
@@ -28,14 +45,21 @@ class BuffetsController < ApplicationController
     end
   end
 
+
   private
   def buffet_params
-    buffet_params = params.require(:buffet).permit(:brand_name, :corporate_name, :registration, :phone_number,
-                                                  :email, :address, :district, :city, :state_code, :zip_code,
+    buffet_params = params.require(:buffet).permit(:brand_name, :corporate_name,
+                                                  :registration, :phone_number,
+                                                  :email, :address, :district, 
+                                                  :city, :state_code, :zip_code,
                                                   :description)
   end
 
   def set_buffet
     @buffet = Buffet.find(params[:id])
+  end
+
+  def payment_methods
+    @payment_methods = PaymentMethod.all
   end
 end
---
+++ spec/system/buffet/buffet_owner_edit_buffet_spec.rb
@@ -0,0 +1,146 @@
+require "rails_helper"
+
+describe "Buffet owner edit his buffet" do
+  it "from show page" do
+    load_payments()
+    user = BuffetOwner.create!(email: 'rafa@el.com', password: "password")
+    buffet = Buffet.create!(brand_name: "Galaxy Buffet", corporate_name: "Buffetys LTDA", registration: "321.543.12/0001-33",
+    phone_number: "99123456789", email: "atendimento@buffyts.com", address: "Rua Estrelas, 123",
+    district: "Sistema Solar", city: "Via lactea", state_code: "AA", zip_code: "99999-999",
+    description: "Um buffet de outro mundo", buffet_owner: user)
+    BuffetPaymentMethod.create!(buffet: buffet, payment_method: PaymentMethod.find(1))
+    
+    login_as user
+    visit root_path
+    click_on "Meu Buffet"
+    click_on "Editar"
+
+    expect(page).to have_content 'Editar Buffet'
+    expect(page).to have_field 'Nome fantasia', with: 'Galaxy Buffet'
+    expect(page).to have_field 'Razão social'
+    expect(page).to have_field 'CNPJ'
+    expect(page).to have_field 'Endereço'
+    expect(page).to have_field 'Cidade'
+    expect(page).to have_field 'Codigo do estado'
+    expect(page).to have_field 'E-mail de contato'
+    expect(page).to have_field 'Telefone de contato'
+    expect(page).to have_field 'Bairro'
+    expect(page).to have_checked_field 'Dinheiro'   
+    expect(page).to have_unchecked_field 'Cartão de Debito'
+    expect(page).to have_unchecked_field 'Cartão de Credito'
+
+  end
+  
+  it "sucessfully" do
+    load_payments()
+    user = BuffetOwner.create!(email: 'rafa@el.com', password: "password")
+    buffet = Buffet.create!(brand_name: "Galaxy Buffet", corporate_name: "Buffetys LTDA", registration: "321.543.12/0001-33",
+    phone_number: "99123456789", email: "atendimento@buffyts.com", address: "Rua Estrelas, 123",
+    district: "Sistema Solar", city: "Via lactea", state_code: "AA", zip_code: "99999-999",
+    description: "Um buffet de outro mundo", buffet_owner: user)
+    BuffetPaymentMethod.create!(buffet: buffet, payment_method: PaymentMethod.find(1))
+    
+    login_as user
+    visit root_path
+    click_on "Meu Buffet"
+    click_on "Editar"
+    fill_in "Nome fantasia",	with: "Buffet da Praça"
+    fill_in "Razão social",	with: "Buffets LTDA"
+    fill_in "CNPJ",	with: "21.543.312/0001-33" 
+    fill_in "Telefone de contato",	with: "00123456789"
+    fill_in "E-mail de contato",	with: "atendimento@buffets.com"
+    fill_in "Endereço",	with: "Rua Estrelas, 312"
+    fill_in "Bairro",	with: "Sistema nao Solar" 
+    fill_in "Cidade",	with: "Via queijo" 
+    fill_in "Codigo do estado",	with: "XX"
+    fill_in "CEP",	with: "99999-990" 
+    uncheck "Dinheiro"
+    check "Cartão de Debito"
+    fill_in "Descrição",	with: "Um buffet de outro mundo"
+    click_on "Atualizar Buffet"
+
+    expect(page).to have_content "Buffet atualizado com sucesso" 
+    expect(page).to have_content "Buffet da Praça"
+    expect(page).to have_content "Buffets LTDA"
+    expect(page).to have_content "21.543.312/0001-33"
+    expect(page).to have_content "00123456789"
+    expect(page).to have_content "atendimento@buffets.com"
+    expect(page).to have_content "Rua Estrelas, 312"
+    expect(page).to have_content "Sistema nao Solar"
+    expect(page).to have_content "Via queijo"
+    expect(page).to have_content "XX"
+    expect(page).to have_content "99999-990"
+    expect(page).to have_content "Um buffet de outro mundo"
+    expect(page).not_to have_content "Dinheiro"
+    expect(page).to have_content "Cartão de Debito"
+  end
+
+  it "with incomplete data" do
+    load_payments()
+    user = BuffetOwner.create!(email: 'rafa@el.com', password: "password")
+    buffet = Buffet.create!(brand_name: "Galaxy Buffet", corporate_name: "Buffetys LTDA", registration: "321.543.12/0001-33",
+    phone_number: "99123456789", email: "atendimento@buffyts.com", address: "Rua Estrelas, 123",
+    district: "Sistema Solar", city: "Via lactea", state_code: "AA", zip_code: "99999-999",
+    description: "Um buffet de outro mundo", buffet_owner: user)
+    BuffetPaymentMethod.create!(buffet: buffet, payment_method: PaymentMethod.find(1))
+    
+    login_as user
+    visit root_path
+    click_on "Meu Buffet"
+    click_on "Editar"
+    fill_in "Nome fantasia",	with: "Buffet da Praça"
+    fill_in "Razão social",	with: "Buffets LTDA"
+    fill_in "CNPJ",	with:''
+    fill_in "Telefone de contato",	with: "00123456789"
+    fill_in "E-mail de contato",	with: "atendimento@buffets.com"
+    fill_in "Endereço",	with: "Rua Estrelas, 312"
+    fill_in "Bairro",	with: "Sistema nao Solar" 
+    fill_in "Cidade",	with: "Via queijo" 
+    fill_in "Codigo do estado",	with: "XX"
+    fill_in "CEP",	with: "99999-990" 
+    uncheck "Dinheiro"
+    check "Cartão de Debito"
+    fill_in "Descrição",	with: "Um buffet de outro mundo"
+    click_on "Atualizar Buffet"
+
+    expect(page).not_to have_content "Buffet atualizado com sucesso" 
+    expect(page).to have_content "Erro ao atualizar Buffet"
+    expect(page).to have_content "CNPJ não pode ficar em branco"
+
+  end
+
+  it "with no payment method" do
+    load_payments()
+    user = BuffetOwner.create!(email: 'rafa@el.com', password: "password")
+    buffet = Buffet.create!(brand_name: "Galaxy Buffet", corporate_name: "Buffetys LTDA", registration: "321.543.12/0001-33",
+    phone_number: "99123456789", email: "atendimento@buffyts.com", address: "Rua Estrelas, 123",
+    district: "Sistema Solar", city: "Via lactea", state_code: "AA", zip_code: "99999-999",
+    description: "Um buffet de outro mundo", buffet_owner: user)
+    BuffetPaymentMethod.create!(buffet: buffet, payment_method: PaymentMethod.find(1))
+    
+    login_as user
+    visit root_path
+    click_on "Meu Buffet"
+    click_on "Editar"
+    fill_in "Nome fantasia",	with: "Buffet da Praça"
+    fill_in "Razão social",	with: "Buffets LTDA"
+    fill_in "CNPJ",	with:'21.543.312/0001-33'
+    fill_in "Telefone de contato",	with: "00123456789"
+    fill_in "E-mail de contato",	with: "atendimento@buffets.com"
+    fill_in "Endereço",	with: "Rua Estrelas, 312"
+    fill_in "Bairro",	with: "Sistema nao Solar" 
+    fill_in "Cidade",	with: "Via queijo" 
+    fill_in "Codigo do estado",	with: "XX"
+    fill_in "CEP",	with: "99999-990" 
+    uncheck "Dinheiro"
+    fill_in "Descrição",	with: "Um buffet de outro mundo"
+    click_on "Atualizar Buffet"
+
+    expect(page).not_to have_content "Buffet atualizado com sucesso" 
+    expect(page).to have_content "Erro ao atualizar Buffet"
+    expect(page).to have_content  "Metodos de Pagamento não pode ficar em branco"
+    
+  end
+  
+  
+end
